49120fcead72ad4c9ff8ec3b1bb318b12afa48cd
Vasyl Yefimenko <rednez@gmail.com>
2025-09-08 19:31:36 +0200

    feat: add ThemeProviders component and update layout to use it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 import Navbar from '@/app/ui/navbar';
-import theme from '@/theme';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import CssBaseline from '@mui/material/CssBaseline';
-import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
-import { ThemeProvider } from '@mui/material/styles';
 import type { Metadata } from 'next';
 import './globals.css';
+import ThemeProviders from './theme-providers';
 
 export const metadata: Metadata = {
   title: 'Sprytno Web Preview',
@@ -20,14 +16,10 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning>
       <body>
-        <InitColorSchemeScript attribute="class" />
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Navbar />
-            {children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+        <ThemeProviders>
+          <Navbar />
+          {children}
+        </ThemeProviders>
       </body>
     </html>
   );
---
+++ src/app/theme-providers.tsx
@@ -0,0 +1,23 @@
+import theme from '@/theme';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import CssBaseline from '@mui/material/CssBaseline';
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+import { ThemeProvider } from '@mui/material/styles';
+
+export default function ThemeProviders({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <InitColorSchemeScript attribute="class" />
+      <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </AppRouterCacheProvider>
+    </>
+  );
+}
