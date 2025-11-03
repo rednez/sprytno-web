10e4631b2198fd5a09ad05deb8c58e3ad86cf3d6
Vasyl Yefimenko <rednez@gmail.com>
2025-11-03 13:37:46 +0100

    fix: add unique key to CreateTaskForm using uuid

--- apps/front/src/app/(authorized)/my-tasks/create/page.tsx
+++ apps/front/src/app/(authorized)/my-tasks/create/page.tsx
@@ -1,4 +1,5 @@
 import { CreateTaskForm } from '@/components/features/tasks';
+import { v4 as uid } from 'uuid';
 
 export default function CreateMyTask() {
   const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY as string;
@@ -8,6 +9,7 @@ export default function CreateMyTask() {
     <div className="mt-2 mb-6">
       <h1 className="text-xl font-medium mb-8">Create Task</h1>
       <CreateTaskForm
+        key={uid()}
         googleMapsApiKey={googleMapsApiKey}
         googleMapsMapId={googleMapsMapId}
       />
